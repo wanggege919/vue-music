2bbb897cb0b8df4a66f0c2ef704bc164631f7b84
wanggege <1914161290@qq.com>
2019-05-15 11:43:47 +0800

    轮播图组件完成

--- src/base/slider/Slider.vue
+++ src/base/slider/Slider.vue
@@ -1,57 +1,177 @@
 <template>
-    <div class="slider">
-        <div class="slider-group">
-            <slot>
-
-            </slot>
-        </div>
-        <div class="dots"></div>
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
+      <slot></slot>
     </div>
+    <div class="dots">
+      <span
+        class="dot"
+        v-for="(item,index) of dots"
+        :key="index"
+        :class="{active: index === currentIndex}"
+      ></span>
+    </div>
+  </div>
 </template>
 <script>
+import BScroll from "better-scroll";
+import { addClass } from "common/js/dom.js";
 export default {
-    
-}
+  name: "Slider",
+  data() {
+    return {
+      dots: [],
+      currentIndex: 0
+    };
+  },
+  props: {//初始化参数，所有可以通过外部传参来改变组件行为的参数都应该用 props
+    loop: {
+      //是否循环轮播
+      type: Boolean,
+      default: true
+    },
+    autoPlay: {
+      //是否自动轮播
+      type: Boolean,
+      default: true
+    },
+    interval: {
+      //轮播的时间间隔
+      type: Number,
+      default: 4000
+    }
+  },
+  methods: {
+    _setSliderWidth(isResize) {
+      this.children = this.$refs.sliderGroup.children;
+      // console.log(this.children)
+      // console.log(this.children.length)
+      let width = 0;
+      let sliderWidth = this.$refs.slider.clientWidth;
+      for (let i = 0; i < this.children.length; i++) {
+        let child = this.children[i];
+        addClass(child, "slider-item");
+        child.style.width = sliderWidth + "px";
+        width += sliderWidth;
+      }
+      if (this.loop && !isResize) {
+        width += 2 * sliderWidth;
+      }
+      this.$refs.sliderGroup.style.width = width + "px";
+    },
+    _initDots() {
+      // this.dots.length = this.children.length
+      this.dots = new Array(this.children.length);
+    },
+    _initSlider() {
+      this.slider = new BScroll(this.$refs.slider, {
+        scrollX: true,//在X轴方向滑动
+        scrollY: false, //在Y轴方向不滑动
+        momentum: false,
+        snap: {
+          loop: this.loop,
+          threshold: 0.3,
+          speed: 400
+        }
+      });
+      this.slider.on("scrollEnd", () => {
+        let pageIndex = this.slider.getCurrentPage().pageX;
+        // if (this.loop) {
+        //   pageIndex -= 1;
+        // }
+        this.currentIndex = pageIndex;
+
+        if(this.autoPlay) {
+          clearTimeout(this.timer)
+          this._play()
+        }
+      });
+      
+    },
+    _play() {
+      let pageIndex = this.currentIndex + 1;
+      this.timer = setTimeout(() => {
+        // this.slider.goToPage(pageIndex, 0, 400);
+        this.slider.next()
+      }, this.interval);
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      //为了保证dom成功渲染，因为页面初始化的时候页面没有真正渲染，或者高度和宽度计算不对
+      this._setSliderWidth();
+      this._initDots();
+      this._initSlider();
+    }, 20) //20ms,因为浏览器的刷新通常是17ms一次
+
+    if (this.autoPlay) {
+      this._play();
+    }
+
+    window.addEventListener('resize', () => {
+        if(!this.slider) { //就是slide还没有初始化的时候
+            return
+        }
+        this._setSliderWidth(true)
+        this.slider.refresh()//重新计算 better-scroll，当 DOM 结构发生变化的时候务必要调用确保滚动的效果正常
+    })
+  }
+};
 </script>
 <style lang="stylus" scoped>
- @import "~common/stylus/variable"
+@import '~common/stylus/variable';
 
-  .slider
-    min-height: 1px
-    .slider-group
-      position: relative
-      overflow: hidden
-      white-space: nowrap
-      .slider-item
-        float: left
-        box-sizing: border-box
-        overflow: hidden
-        text-align: center
-        a
-          display: block
-          width: 100%
-          overflow: hidden
-          text-decoration: none
-        img
-          display: block
-          width: 100%
-    .dots
-      position: absolute
-      right: 0
-      left: 0
-      bottom: 12px
-      transform: translateZ(1px)
-      text-align: center
-      font-size: 0
-      .dot
-        display: inline-block
-        margin: 0 4px
-        width: 8px
-        height: 8px
-        border-radius: 50%
-        background: $color-text-l
-        &.active
-          width: 20px
-          border-radius: 5px
-          background: $color-text-ll
+.slider {
+  min-height: 1px;
+
+  .slider-group {
+    position: relative;
+    overflow: hidden;
+    white-space: nowrap;
+
+    .slider-item {
+      float: left;
+      box-sizing: border-box;
+      overflow: hidden;
+      text-align: center;
+
+      a {
+        display: block;
+        width: 100%;
+        overflow: hidden;
+        text-decoration: none;
+      }
+
+      img {
+        display: block;
+        width: 100%;
+      }
+    }
+  }
+
+  .dots {
+    position: absolute;
+    right: 0;
+    left: 0;
+    bottom: 12px;
+    transform: translateZ(1px);
+    text-align: center;
+    font-size: 0;
+
+    .dot {
+      display: inline-block;
+      margin: 0 4px;
+      width: 8px;
+      height: 8px;
+      border-radius: 50%;
+      background: $color-text-l;
+
+      &.active {
+        width: 20px;
+        border-radius: 5px;
+        background: $color-text-ll;
+      }
+    }
+  }
+}
 </style>
--- src/components/recommend/Recommend.vue
+++ src/components/recommend/Recommend.vue
@@ -1,14 +1,18 @@
 <template>
     <div class="recommend">
         <div class="recommend-content">
-          <div class="slider-wrapper">
-            <Slider>
-              <div v-for="item of recommends" :key="item.id">
-                <a :href="item.linkUrl">
-                  <img :src="item.picUrl" alt="">
-                </a>
-              </div>
-            </Slider>
+          <div v-if="recommends.length" class="slider-wrapper">
+            <!--为什么加v-if，因为recommends请求是一个异步操作，需要时间，可能数据还没回来，
+            轮播组件的mounted中的初始化就已经完成了，所以加上v-if，数据不回来，就把这些dom隐藏掉-->
+            <div class="slider-content">
+              <Slider>
+                <div v-for="item of recommends" :key="item.id">
+                  <a :href="item.linkUrl">
+                    <img :src="item.picUrl" alt="">
+                  </a>
+                </div>
+              </Slider>
+            </div>
           </div>
           <div class="recommend-list">
             <h1 class="list-title">
@@ -48,60 +52,60 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-  //  @import "~common/stylus/variable"
+   @import "~common/stylus/variable"
 
-  // .recommend
-  //   position: fixed
-  //   width: 100%
-  //   top: 88px
-  //   bottom: 0
-  //   .recommend-content
-  //     height: 100%
-  //     overflow: hidden
-  //     .slider-wrapper
-  //       position: relative
-  //       width: 100%
-  //       height: 0
-  //       padding-top: 40%
-  //       overflow: hidden
-  //       .slider-content
-  //         position: absolute
-  //         top: 0
-  //         left: 0
-  //         width: 100%
-  //         height: 100%
-  //     .recommend-list
-  //       .list-title
-  //         height: 65px
-  //         line-height: 65px
-  //         text-align: center
-  //         font-size: $font-size-medium
-  //         color: $color-theme
-  //       .item
-  //         display: flex
-  //         box-sizing: border-box
-  //         align-items: center
-  //         padding: 0 20px 20px 20px
-  //         .icon
-  //           flex: 0 0 60px
-  //           width: 60px
-  //           padding-right: 20px
-  //         .text
-  //           display: flex
-  //           flex-direction: column
-  //           justify-content: center
-  //           flex: 1
-  //           line-height: 20px
-  //           overflow: hidden
-  //           font-size: $font-size-medium
-  //           .name
-  //             margin-bottom: 10px
-  //             color: $color-text
-  //           .desc
-  //             color: $color-text-d
-  //     .loading-container
-  //       position: absolute
-  //       width: 100%
-  //       top: 50%
-  //       transform: translateY(-50%)
+  .recommend
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .recommend-content
+      height: 100%
+      overflow: hidden
+      .slider-wrapper
+        position: relative
+        width: 100%
+        height: 0
+        padding-top: 40%
+        overflow: hidden
+        .slider-content
+          position: absolute
+          top: 0
+          left: 0
+          width: 100%
+          height: 100%
+      .recommend-list
+        .list-title
+          height: 65px
+          line-height: 65px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-theme
+        .item
+          display: flex
+          box-sizing: border-box
+          align-items: center
+          padding: 0 20px 20px 20px
+          .icon
+            flex: 0 0 60px
+            width: 60px
+            padding-right: 20px
+          .text
+            display: flex
+            flex-direction: column
+            justify-content: center
+            flex: 1
+            line-height: 20px
+            overflow: hidden
+            font-size: $font-size-medium
+            .name
+              margin-bottom: 10px
+              color: $color-text
+            .desc
+              color: $color-text-d
+      .loading-container
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
 </style>
